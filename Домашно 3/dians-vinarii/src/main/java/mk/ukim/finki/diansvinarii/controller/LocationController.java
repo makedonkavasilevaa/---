06847244c7cf846567b1538b728be27d9d12180b
makedonkavasilevaa <99400411+makedonkavasilevaa@users.noreply.github.com>
2023-12-20 12:16:27 +0100

    LocationController GoogleMaps integration for routing

--- Домашно 3/dians-vinarii/src/main/java/mk/ukim/finki/diansvinarii/controller/LocationController.java
+++ Домашно 3/dians-vinarii/src/main/java/mk/ukim/finki/diansvinarii/controller/LocationController.java
@@ -1,6 +1,7 @@
 package mk.ukim.finki.diansvinarii.controller;
 
 import ch.qos.logback.core.model.Model;
+import com.fasterxml.jackson.annotation.JsonAlias;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,12 +11,47 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class LocationController {
+    static class LatLng {
+        @JsonAlias({"lat", "latitude"})
+        private double lat;
+
+        @JsonAlias({"lng", "longitude"})
+        private double lng;
+
+        // getters and setters
+        public double getLat() {
+            return lat;
+        }
+        public void setLat(double lat) {
+            this.lat = lat;
+        }
+        public double getLng() {
+            return lng;
+        }
+        public void setLng(double lng) {
+            this.lng = lng;
+        }
+    }
 
     @PostMapping("/chooseLocation")
-    public ResponseEntity<String> handleChosenLocation(@RequestBody Map<String, String> requestBody) {
-        String location = requestBody.get("location");
-        // Validate and process the location as needed
+    public ResponseEntity<String> handleChosenLocation(@RequestBody LatLng latLng) {
+        double latitude = latLng.getLat();
+        double longitude = latLng.getLng();
 
-        return ResponseEntity.ok("Successfully chosen location: " + location);
+        // Validate and process the latitude and longitude as needed
+        String response = "Successfully chosen location. Latitude: " + latitude + ", Longitude: " + longitude;
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/navigate")
+    public ResponseEntity<String> navigateToChosenLocation(@RequestParam double lat, @RequestParam double lng) {
+        String googleMapsLink = generateGoogleMapsLink(lat, lng);
+        return ResponseEntity.ok(googleMapsLink);
+    }
+
+    private String generateGoogleMapsLink(double lat, double lng) {
+        // You can customize this link based on your requirements
+        return "https://www.google.com/maps/dir/?api=1&destination=" + lat + "," + lng;
     }
 }
