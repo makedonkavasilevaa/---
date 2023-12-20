90f137164a4c8ea50d95f7af88f30b9ce3204e4e
makedonkavasilevaa <99400411+makedonkavasilevaa@users.noreply.github.com>
2023-12-20 12:19:59 +0100

    LocationController GoogleMaps integration for routing update

--- Домашно 3/dians-vinarii/src/main/java/mk/ukim/finki/diansvinarii/controller/LocationController.java
+++ Домашно 3/dians-vinarii/src/main/java/mk/ukim/finki/diansvinarii/controller/LocationController.java
@@ -11,6 +11,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class LocationController {
+
     static class LatLng {
         @JsonAlias({"lat", "latitude"})
         private double lat;
@@ -19,39 +20,39 @@ public class LocationController {
         private double lng;
 
         // getters and setters
+
         public double getLat() {
             return lat;
         }
+
         public void setLat(double lat) {
             this.lat = lat;
         }
+
         public double getLng() {
             return lng;
         }
+
         public void setLng(double lng) {
             this.lng = lng;
         }
     }
 
-    @PostMapping("/chooseLocation")
-    public ResponseEntity<String> handleChosenLocation(@RequestBody LatLng latLng) {
-        double latitude = latLng.getLat();
-        double longitude = latLng.getLng();
+    @PostMapping("/navigate")
+    public ResponseEntity<String> navigateToChosenLocation(@RequestBody Map<String, LatLng> locationData) {
+        LatLng currentLocation = locationData.get("currentLocation");
+        LatLng desiredLocation = locationData.get("desiredLocation");
 
-        // Validate and process the latitude and longitude as needed
-        String response = "Successfully chosen location. Latitude: " + latitude + ", Longitude: " + longitude;
+        // Validate and process the locations as needed
+        String response = generateGoogleMapsLink(currentLocation, desiredLocation);
 
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/navigate")
-    public ResponseEntity<String> navigateToChosenLocation(@RequestParam double lat, @RequestParam double lng) {
-        String googleMapsLink = generateGoogleMapsLink(lat, lng);
-        return ResponseEntity.ok(googleMapsLink);
-    }
-
-    private String generateGoogleMapsLink(double lat, double lng) {
+    private String generateGoogleMapsLink(LatLng currentLocation, LatLng desiredLocation) {
         // You can customize this link based on your requirements
-        return "https://www.google.com/maps/dir/?api=1&destination=" + lat + "," + lng;
+        return "https://www.google.com/maps/dir/?api=1&origin=" +
+                currentLocation.getLat() + "," + currentLocation.getLng() +
+                "&destination=" + desiredLocation.getLat() + "," + desiredLocation.getLng();
     }
 }
